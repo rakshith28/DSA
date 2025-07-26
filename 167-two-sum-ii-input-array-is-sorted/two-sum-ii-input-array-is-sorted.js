ce5b83f43fe9f5fcb8aa7540c50846912cf95bf9
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-07-26 10:12:05 +0530

    Time: 3 ms (37.58%) | Memory: 57 MB (15.60%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -3,19 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
-    let left = 0;
-    let right = numbers.length - 1;
-
-    while (left < right) {
-        let total = numbers[left] + numbers[right];
-
-        if (total === target) {
-            return [left + 1, right + 1];
-        } else if (total > target) {
-            right--;
-        } else {
-            left++;
-        }
-    }    
+var twoSum = function (numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+    while (i < j) {
+         const sum = numbers[i] + numbers[j]
+         if(target === sum ){
+            return [i+1, j+1]
+         }else if(target < sum){
+              j-- 
+         }else{
+           i++
+         }
+    }
 };
+
+
