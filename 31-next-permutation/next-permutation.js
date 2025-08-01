e7b41a1bd2ec4e163ef661cdc1bd59b448b68425
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-02 00:06:59 +0530

    Time: 1 ms (24.78%) | Memory: 54.5 MB (43.55%) - LeetSync

--- 31-next-permutation/next-permutation.js
+++ 31-next-permutation/next-permutation.js
@@ -3,31 +3,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    let i = nums.length - 2;
-    
-    // Step 1: Find the first decreasing element from the right
-    while (i >= 0 && nums[i] >= nums[i + 1]) {
-        i--;
+    let i = nums.length - 2
+    while(i >= 0 && nums[i] >= nums[i+1]){
+        i--
     }
-
-    if (i >= 0) {
-        let j = nums.length - 1;
-        // Step 2: Find the next larger element to the right
-        while (nums[j] <= nums[i]) {
-            j--;
-        }
-        // Step 3: Swap
-        [nums[i], nums[j]] = [nums[j], nums[i]];
+    if(i >= 0){
+       let j  = nums.length - 1
+       while(j>=0 && nums[j] <= nums[i]){
+        j--
+       }
+       [nums[i], nums[j]] = [nums[j], nums[i]]
     }
-
-    // Step 4: Reverse the subarray to the right of `i`
-    let left = i + 1;
-    let right = nums.length - 1;
-    while (left < right) {
-        [nums[left], nums[right]] = [nums[right], nums[left]];
-        left++;
-        right--;
-    }
-
-    return nums;
+    return reverse(nums, i+1, nums.length - 1)
 };
+
+const reverse = (nums, start, end) => {
+    while(start < end){
+    [nums[start], nums[end]]  = [nums[end], nums[start]]
+    start++
+    end--
+    }
+}
