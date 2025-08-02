90cac4ff0cdccf1dd66dddaa3b9c263c2b91afb8
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-03 00:07:33 +0530

    Time: 1 ms (87.78%) | Memory: 55.3 MB (37.87%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,16 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   const indexMap = {}
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (indexMap.hasOwnProperty(complement)) {
-      return [indexMap[complement], i];
+   const obj = new Map()
+   for(let i = 0; i < nums.length; i++){
+    let comp = target - nums[i]
+    if(obj.has(comp)){
+        return [i, obj.get(comp)]
     }
-    indexMap[nums[i]] = i;
-  }
-
-  return null;
+    obj.set(nums[i], i)
+   }
 }
 
 
