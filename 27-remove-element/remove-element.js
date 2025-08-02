5887b9334f84f4bcf096296cba544201a5a16056
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-02 23:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (81.10%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -1,21 +1,17 @@
-/**
- * @param {number[]} nums
- * @param {number} val
- * @return {number}
- */
-var removeElement = function(nums, val) {
-    let l = 0;
-    let r = nums.length - 1;
-    while (l <= r) {
-        if (nums[l] === val) {
-            let temp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = temp;
-            r--; 
-        } else {
-            l++; 
-        }
-    }
-    return l; 
-}
 
+var removeElement = function(nums, val) {
+   let i = 0;
+   let j = nums.length - 1
+   while(i <= j){
+      if(nums[i] === val){
+         let temp = nums[i];
+         nums[i] = nums[j];
+         nums[j] = temp;
+         j--
+      }else{
+           i++
+      }
+    
+   }
+   return i
+}
