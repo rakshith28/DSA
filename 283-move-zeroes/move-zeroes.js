5378b1b3deb6c97f501f9b8e78f3919d11cbd522
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-02 20:07:11 +0530

    Time: 1 ms (77.91%) | Memory: 60.9 MB (24.68%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -1,39 +1,38 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
-var moveZeroes = function(nums) {
-  let nonZeroElement  = 0;
-  for(let i = 0; i < nums.length; i++){
-    if(nums[i] !== 0){
-        nums[nonZeroElement] = nums[i]
-        nonZeroElement++
-    }
-  }
-  for(let j = nonZeroElement; j < nums.length; j++){
-    nums[j]  = 0;
-  }
-};
-
-
-
-
-
- //    */
+// /**
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
 // var moveZeroes = function(nums) {
-//   let l = 0;
-//   let r = 0;
-//   let temp
-//   while(l < nums.length && r < nums.length){
-//     if(nums[l] !== 0 && l < r){
-//         l++
-//     }else{
-//         temp = nums[l]
-//         nums[l] = nums[r]
-//         nums[r] = temp
-//         r++
+//   let nonZeroElement  = 0;
+//   for(let i = 0; i < nums.length; i++){
+//     if(nums[i] !== 0){
+//         nums[nonZeroElement] = nums[i]
+//         nonZeroElement++
 //     }
 //   }
-//   return nums
+//   for(let j = nonZeroElement; j < nums.length; j++){
+//     nums[j]  = 0;
+//   }
 // };
+
+
+
+
+
+var moveZeroes = function(nums) {
+  let l = 0;
+  let r = 0;
+  let temp
+  while(l < nums.length && r < nums.length){
+    if(nums[l] !== 0 && l < r){
+        l++
+    }else{
+        temp = nums[l]
+        nums[l] = nums[r]
+        nums[r] = temp
+        r++
+    }
+  }
+  return nums
+};
 
