4004af99f94f7f9c4ac885218fbdc21568a95bf6
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-02 19:43:45 +0530

    Time: 2 ms (53.67%) | Memory: 60.2 MB (65.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,18 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  let l = 0;
-  let r = 0;
-  let temp
-  while(l < nums.length && r < nums.length){
-    if(nums[l] !== 0 && l < r){
-        l++
-    }else{
-        temp = nums[l]
-        nums[l] = nums[r]
-        nums[r] = temp
-        r++
-    }
+  let i = 0;
+  let j = 0;
+  while( i < nums.length && j < nums.length ){
+     if(nums[i] !== 0 && i < j){
+         i++
+     }else{
+        [nums[i], nums[j]] = [nums[j], nums[i]] 
+        j++
+     }
   }
   return nums
 };
@@ -23,4 +20,21 @@ var moveZeroes = function(nums) {
 
 
 
-    
+ //    */
+// var moveZeroes = function(nums) {
+//   let l = 0;
+//   let r = 0;
+//   let temp
+//   while(l < nums.length && r < nums.length){
+//     if(nums[l] !== 0 && l < r){
+//         l++
+//     }else{
+//         temp = nums[l]
+//         nums[l] = nums[r]
+//         nums[r] = temp
+//         r++
+//     }
+//   }
+//   return nums
+// };
+
