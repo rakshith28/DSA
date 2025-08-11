5cff59175e6da0cf610a3608a4e0bacf5592e16a
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-11 12:20:32 +0530

    Time: 173 ms (14.29%) | Memory: 67.6 MB (0.00%) - LeetSync

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minKBitFlips = function(nums, k) {
+    const dQueue = []
+    let flip = 0
+    let n = nums.length
+    for(let i = 0; i < n; i++){
+        if(dQueue?.length && dQueue[0] < i ){
+            dQueue.shift();
+        }
+        if(dQueue?.length % 2 === nums[i]){
+            if(i+ k - 1 >= n){
+                return -1
+            }
+            dQueue.push(i+ k - 1);
+            flip++
+        }
+
+    }
+    return flip
+};
+
+
+
+
