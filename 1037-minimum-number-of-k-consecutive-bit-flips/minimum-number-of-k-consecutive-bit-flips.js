047a71f8c0d9589a7190302335eaed61169d863c
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-11 12:25:36 +0530

    Time: 167 ms (28.57%) | Memory: 66.9 MB (0.00%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.js
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.js
@@ -6,13 +6,12 @@
 var minKBitFlips = function(nums, k) {
     let dQueue = []
     let flip = 0
-    let n = nums.length
-    for(let i = 0; i < n; i++){
+    for(let i = 0; i < nums.length; i++){
         if(dQueue?.length && dQueue[0] < i ){
             dQueue.shift();
         }
         if(dQueue?.length % 2 === nums[i]){
-            if(i+ k - 1 >= n){
+            if(i+ k - 1 >= nums.length){
                 return -1
             }
             dQueue.push(i+ k - 1);
