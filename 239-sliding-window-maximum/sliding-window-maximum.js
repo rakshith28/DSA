92efed2c136d1bc056ab95ceb590e406dc175046
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-16 23:57:27 +0530

    Time: 425 ms (53.10%) | Memory: 88.3 MB (46.20%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -4,24 +4,25 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
-   const deque = [];
-   const results = [];
-   for(let  i = 0;  i < nums.length; i++){
+    const deque = []
+    const results = []
+    for(let i = 0; i < nums.length; i++){
 
       if(deque.length && deque[0] <= i - k){
-        deque.shift()
+        deque.shift();
       }
 
       while(deque.length && nums[deque[deque.length - 1]] < nums[i]){
-        deque.pop()
+        deque.pop();
       }
 
-        deque.push(i)
+      deque.push(i);
 
-    if(i >= k - 1){
+     if(i >= k - 1){
         results.push(nums[deque[0]])
+     }
     }
-   } 
-   return results;
+
+    return results
 };
 
