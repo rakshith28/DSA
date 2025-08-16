3d7714de817eda0b4672c64e3cc690d983dd6e2f
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-16 14:16:23 +0530

    Time: 25 ms (41.18%) | Memory: 60.1 MB (45.07%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -1,29 +1,49 @@
-var checkInclusion = function(s1, s2) {
-   let k = s1.length;
-   let i = 0;
-   while (i + k <= s2.length) {
-     let subArray = s2.slice(i, i + k);
-     let val = checkVal(s1, subArray);
-     if (val) {
-        return true;
-     }
-     i++;
-   }
-   return false;
+var checkInclusion = function (s1, s2) {
+    const k = s1.length, n = s2.length;
+    if (k > n) return false;
+    let need = new Map();
+    let window = new Map();
+
+    for (let i = 0; i < s1.length; i++) {
+        const ch = s1[i];
+        need.set(ch, (need.get(ch) || 0) + 1)
+    }
+
+    for (let i = 0; i < k; i++) {
+        const ch = s2[i]
+        window.set(ch, (window.get(ch) || 0) + 1);
+    }
+
+    if (match(need, window)) {
+        return true
+    }
+
+    for (let i = k; i < n; i++) {
+
+        let addChar = s2[i];
+        window.set(addChar, (window.get(addChar) || 0) + 1);
+
+        let removeChar = s2[i - k];
+        window.set(removeChar, window.get(removeChar) - 1);
+
+        if (window.get(removeChar) === 0) {
+            window.delete(removeChar); // clean up
+        }
+
+        if (match(need, window)) return true;
+
+
+    }
+    return false;
+
 };
 
-const checkVal = (s1, s2) => {
-  const map1 = new Map();
-  const map2 = new Map();
-  for (let ch of s1) {
-    map1.set(ch, (map1.get(ch) || 0) + 1);
-  }
-  for (let ch of s2) {
-    map2.set(ch, (map2.get(ch) || 0) + 1);
-  }
-  if (map1.size !== map2.size) return false;
-  for (let [key, val] of map1) {
-    if (map2.get(key) !== val) return false;
-  }
-  return true;
-};
+
+const match = (m1, m2) => {
+    if (m1.size !== m2.size) return false
+    for (let [key, val] of m1) {
+        if (m2.get(key) !== val) return false
+    }
+    return true
+}
+
