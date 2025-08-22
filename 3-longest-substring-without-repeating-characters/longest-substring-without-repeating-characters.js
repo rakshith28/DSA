9fa9aca5e7ca7b9795e996883ce1e9246fa74779
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-22 14:53:18 +0530

    Time: 8 ms (50.75%) | Memory: 59.7 MB (29.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-   let j = 0; 
-   let results = 0
-   let check = new Map()
-   for(let i = 0; i < s.length; i++){
-       while(check.has(s[i])){
-        check.delete(s[j])
-        j++
-       }
-       let width = i - j + 1
-       check.set(s[i])
-       results = Math.max(results, width)
+   let r = 0;
+   let l= 0;
+   let maxCount = 0;
+   let checkMap = new Map();
+   while(r < s.length){
+    while(checkMap.has(s[r])){
+       checkMap.delete(s[l]);
+       l++
+    }
+       let width = r - l + 1;
+       checkMap.set(s[r])
+       maxCount = Math.max(maxCount, width)
+       r++
    }
-   return results
+   return maxCount
 };
-
-
