660f41205b51812b30b3c5966d0e370aa23d1988
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-27 12:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 58.6 MB (23.33%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.js
+++ 724-find-pivot-index/find-pivot-index.js
@@ -3,16 +3,14 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    let totalSum = nums.reduce((a, b) => a + b, 0); // sum of all elements
-    let leftSum = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        let rightSum = totalSum - leftSum - nums[i];
-        if (leftSum === rightSum) {
-            return i; // found pivot index
-        }
-        leftSum += nums[i];
-    }
-
-    return -1; // if no pivot index exists
+   let left = 0
+   const sum  = nums.reduce((a, b) => a + b, 0)
+   for(let i = 0; i < nums.length; i++){
+     const total = sum - nums[i] - left
+     if(total === left){
+       return i
+      }
+     left += nums[i]  
+   }  
+   return -1  
 };
