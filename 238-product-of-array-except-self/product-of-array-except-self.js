3b8468d4e926e9e3940af1451a08f848482fe7df
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-08-27 13:23:38 +0530

    Time: 2 ms (96.90%) | Memory: 71.2 MB (44.61%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+     let res = new Array(nums.length)
+     let prefix = 1;
+    let suffix = 1;
+    for(let i = 0; i < nums.length; i++){
+         res[i] = prefix
+         prefix *= nums[i]
+    }
+      for(let i = nums.length - 1; i >= 0; i--){
+         res[i] *= suffix
+         suffix *= nums[i]
+    }
+    return res
+};
+
