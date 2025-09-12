79d03c6c50443c2b483c8d92190766a86187aa29
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-09-12 23:56:06 +0530

    Time: 13 ms (93.03%) | Memory: 74.7 MB (98.97%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -4,13 +4,14 @@
  */
 var dailyTemperatures = function(temp) {
     let stack = []
-    let ng = []
+    let ng = new Array(temp.length).fill(0)
     for (let i = temp.length - 1; i >= 0; i--) {
         while (stack.length > 0 && temp[stack[stack.length - 1]] <= temp[i]) {
             stack.pop()
         }
-            ng[i] = stack[stack.length - 1] - i || 0
-        
+        if (stack.length > 0) {
+            ng[i] = stack[stack.length - 1] - i
+        } 
         stack.push(i)
     }
     return ng
