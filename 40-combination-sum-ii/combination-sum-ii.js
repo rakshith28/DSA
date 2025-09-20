c50384586aafb641be14c7829e9adbf95a800bbd
rakshith28 <61915226+rakshith28@users.noreply.github.com>
2025-09-20 15:09:36 +0530

    Time: 1 ms (97.42%) | Memory: 58.7 MB (12.55%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    const result = [];
+    candidates.sort((a, b) => a - b);
+
+    const backtrack = (start, target, path) => {
+        if (target === 0) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] === candidates[i - 1]) continue;
+
+            if (candidates[i] > target) break;
+
+            path.push(candidates[i]);
+            backtrack(i + 1, target - candidates[i], path);
+            path.pop();
+        }
+    };
+    backtrack(0, target, []);
+    return result;
+};
